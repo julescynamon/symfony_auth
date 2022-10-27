73faee6e0e6ca22f3d0f6938a83afc6d1f230062
cynamon jules <julescweb@mail.com>
2022-10-27 10:07:19 +0200

    mise en place de la connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/inscription', name: 'inscription')]
-    public function index(): Response
+    public function inscription(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+
+
         return $this->render('security/inscription.html.twig', [
-            'controller_name' => 'SecurityController',
+            'form' => $userForm->createView()
+        ]);
+    }
+
+    #[Route('/connexion', name: 'connexion')]
+    public function connexion(AuthenticationUtils $authenticationUtils): Response
+    {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $userName = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/connexion.html.twig', [
+            'error' => $error,
+            'userName' => $userName
         ]);
     }
 }
